eabefd03a5d4d4aca61bc8b22b8ea32a93c4a7b6
Alex King <alex.king@and.digital>
2020-11-15 16:00:43 +0000

    Commit milestone-1 solution

---
+++ Assets/Scripts/GameBlockController.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBlockController : MonoBehaviour
+{
+    public AudioSource BlockDropAudioSource;
+
+    public bool isSettled { get; set; }
+    public bool isGrabbed { get; set; }
+
+    private readonly int DEFAULT_FRAMERATE = 72;
+    private readonly float BOTTOM_Y = 1.5f;
+    private readonly float LEFT_WALL_X = -5.5f;
+    private readonly float RIGHT_WALL_X = 5.5f;
+
+    private int counter = 0;
+    private Quaternion lastControllerRotation = Quaternion.identity;
+    private Vector3 lastLateralUpdateLocation = Vector3.zero;
+    private Vector3 lastDownUpdateLocation = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isSettled = false;
+        isGrabbed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isSettled)
+        {
+            // Handle key presses
+            if (
+                (Input.GetKeyDown(KeyCode.RightArrow) || OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger)) 
+                && !WouldCollideWithWall(1)
+            )
+            {
+                transform.Translate(new Vector3(1, 0, 0), Space.World);
+            } 
+            else if (
+                (Input.GetKeyDown(KeyCode.LeftArrow) || OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger))
+                && !WouldCollideWithWall(-1)
+            )
+            {
+                transform.Translate(new Vector3(-1, 0, 0), Space.World);
+            } 
+            else if (Input.GetKeyDown(KeyCode.DownArrow) || OVRInput.GetDown(OVRInput.RawButton.B)) 
+            {
+                transform.Translate(new Vector3(0, -1, 0), Space.World);
+                BlockDropAudioSource.Play();
+            } 
+            else if (Input.GetKeyDown(KeyCode.Space) || OVRInput.GetDown(OVRInput.RawButton.A))
+            {  
+                RotateClockwise();
+            }
+            else if (OVRInput.GetDown(OVRInput.RawButton.RHandTrigger))
+            {
+                isGrabbed = true;
+                lastControllerRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+                lastLateralUpdateLocation = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+                lastDownUpdateLocation = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+            }
+            else if (OVRInput.GetUp(OVRInput.RawButton.RHandTrigger))
+            {
+                isGrabbed = false;
+                SnapRotation();
+            }
+
+            if (isGrabbed)
+            {
+                Quaternion currentRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+                Vector3 currentLocation = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+
+                // Rotation
+                Vector3 eulers = new Vector3(0, 0, currentRotation.eulerAngles.z - lastControllerRotation.eulerAngles.z);
+                transform.Rotate(eulers, Space.World);
+                lastControllerRotation = currentRotation;
+
+                // X-translation
+                if (currentLocation.x - lastLateralUpdateLocation.x > 0.05 && !WouldCollideWithWall(1))
+                {
+                    transform.Translate(new Vector3(1, 0, 0), Space.World);
+                    lastLateralUpdateLocation = currentLocation;
+                } 
+                else if (currentLocation.x - lastLateralUpdateLocation.x < -0.05 && !WouldCollideWithWall(-1))
+                {
+                    transform.Translate(new Vector3(-1, 0, 0), Space.World);
+                    lastLateralUpdateLocation = currentLocation;
+                }
+
+                // Y-translation
+                if (currentLocation.y - lastDownUpdateLocation.y < -0.03)
+                {
+                    transform.Translate(new Vector3(0, -1, 0), Space.World);
+                    lastDownUpdateLocation = currentLocation;
+                }
+            }
+
+            // Move on timer
+            if (counter >= DEFAULT_FRAMERATE) 
+            {
+                counter = 0;
+                transform.Translate(new Vector3(0, -1, 0), Space.World);
+                BlockDropAudioSource.Play();
+            } 
+            else 
+            {
+                counter++;
+            }
+        }
+    }
+
+    public void TrySettle(List<GameObject> settledBlocks)
+    {
+        for (int i = 0; i < transform.childCount - 1; i++)
+        {
+            Vector3 subBlockPos = transform.GetChild(i).position;
+
+            if (subBlockPos.y <= BOTTOM_Y)
+            {
+                isSettled = true;
+            }
+
+            for (int j = 0; j < settledBlocks.Count; j++)
+            {
+                Vector3 settledBlockPos = settledBlocks[j].transform.position;
+
+                if (
+                    Mathf.Floor(subBlockPos.x) == Mathf.Floor(settledBlockPos.x) 
+                    && Mathf.Floor(subBlockPos.y - 1) == Mathf.Floor(settledBlockPos.y)
+                )
+                {
+                    isSettled = true;
+                }
+            }
+        }
+    }
+
+    public void SnapRotation()
+    {
+        float zAngle = (float) Mathf.RoundToInt(transform.rotation.eulerAngles.z / 90) * 90;
+        Quaternion rot = Quaternion.AngleAxis(zAngle, Vector3.forward);
+        transform.rotation = rot;
+    }
+
+    protected bool WouldCollideWithWall(int xChange)
+    {
+        for (int i = 0; i < transform.childCount - 1; i++)
+        {
+            if (xChange < 0 && transform.GetChild(i).position.x - 1 == LEFT_WALL_X)
+            {
+                return true;
+            }
+            else if (xChange > 0 && transform.GetChild(i).position.x + 1 == RIGHT_WALL_X)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    protected void RotateClockwise()
+    {
+        // TODO: Rotation collision detection
+        transform.Rotate(new Vector3(0, 0, 90));
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject TBlockPrefab;
+    public GameObject OBlockPrefab;
+    public GameObject JBlockPrefab;
+    public GameObject LBlockPrefab;
+    public GameObject IBlockPrefab;
+    public GameObject SBlockPrefab;
+    public GameObject ZBlockPrefab;
+    public GameObject LinesClearedValue;
+    public GameObject GameOverText;
+    public AudioSource BgmAudioSource;
+    public AudioSource LineClearAudioSource;
+
+    private GameObject liveBlock;
+    private bool gameStarted;
+    private List<GameObject> settledBlocks;
+    private int linesCleared = 0;
+    private readonly int BOARD_WIDTH = 10;
+    private readonly int MAX_SETTLED_Y = 19;
+    private readonly Vector3 BLOCK_SPAWN_POSITION = new Vector3(0.5f, 20.5f, 10);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameStarted = false;
+        settledBlocks = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Start game
+        if ((Input.GetKeyDown(KeyCode.Return) || OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger)) && gameStarted == false) 
+        {
+            gameStarted = true;
+            BgmAudioSource.Play();
+            GameOverText.SetActive(false);
+            InstantiateRandomBlock();
+        }
+
+        if (gameStarted)
+        {
+            TryGameOver();
+
+            if (liveBlock)
+            {
+                liveBlock.GetComponent<GameBlockController>().TrySettle(settledBlocks);
+
+                // Settle block
+                if (liveBlock.GetComponent<GameBlockController>().isSettled) 
+                {
+                    liveBlock.GetComponent<GameBlockController>().SnapRotation();
+
+                    for (int i = 0; i < liveBlock.transform.childCount - 1; i++)
+                    {
+                        settledBlocks.Add(liveBlock.transform.GetChild(i).gameObject);
+                    }
+
+                    ClearCompletedLines();
+                    InstantiateRandomBlock();
+                }
+            }
+        }
+    }
+
+    private void ClearCompletedLines()
+    {
+        // Count lines into dictionary
+        Dictionary<int, List<Transform>> blockLines = new Dictionary<int, List<Transform>>();
+        for (int i = 0; i < settledBlocks.Count; i++)
+        {
+            Transform subBlock = settledBlocks[i].transform;
+            int subBlockY = (int) Mathf.Floor(subBlock.position.y);
+            if (!blockLines.ContainsKey(subBlockY))
+            {
+                blockLines.Add(subBlockY, new List<Transform>());
+            }
+            blockLines[subBlockY].Add(subBlock);
+        }
+        settledBlocks.Clear();
+
+        // Delete completed lines
+        IDictionaryEnumerator dictEnum = blockLines.GetEnumerator();
+        while (dictEnum.MoveNext())
+        {
+            if ((dictEnum.Value as List<Transform>).Count >= BOARD_WIDTH)
+            {
+                LineClearAudioSource.Play();
+                linesCleared++;
+                LinesClearedValue.GetComponent<UnityEngine.UI.Text>().text = linesCleared.ToString();
+
+                foreach (Transform block in (dictEnum.Value as List<Transform>))
+                {
+                    Destroy(block.gameObject);
+                }
+
+                // Shift above blocks down
+                foreach (int key in blockLines.Keys)
+                {
+                    if (key > (int) dictEnum.Key)
+                    {
+                        foreach (Transform block in blockLines[key])
+                        {
+                            block.Translate(new Vector3(0, -1, 0), Space.World);
+                        }
+                    }
+                }
+            }
+            else 
+            {
+                // Add processed lines back to list
+                foreach (Transform block in (dictEnum.Value as List<Transform>))
+                {
+                    settledBlocks.Add(block.gameObject);
+                }
+            }
+        }
+    }
+
+    private void TryGameOver()
+    {
+        for (int i = 0; i < settledBlocks.Count; i++)
+        {
+            Transform subBlock = settledBlocks[i].transform;
+            int subBlockY = (int) Mathf.Floor(subBlock.position.y);
+            if (subBlockY >= MAX_SETTLED_Y)
+            {
+                gameStarted = false;
+                BgmAudioSource.Stop();
+                GameOverText.SetActive(true);
+                liveBlock.GetComponent<GameBlockController>().isSettled = true;
+            }
+        }
+    }
+
+    private void InstantiateRandomBlock()
+    {
+        switch (Mathf.RoundToInt(Random.Range(-0.49f, 6.49f)))
+        {
+            case 0:
+                liveBlock = Instantiate(TBlockPrefab, BLOCK_SPAWN_POSITION, Quaternion.identity) as GameObject;
+                break;
+            case 1:
+                liveBlock = Instantiate(OBlockPrefab, BLOCK_SPAWN_POSITION, Quaternion.identity) as GameObject;
+                break;
+            case 2:
+                liveBlock = Instantiate(JBlockPrefab, BLOCK_SPAWN_POSITION, Quaternion.identity) as GameObject;
+                break;
+            case 3:
+                liveBlock = Instantiate(LBlockPrefab, BLOCK_SPAWN_POSITION, Quaternion.identity) as GameObject;
+                break;
+            case 4:
+                liveBlock = Instantiate(IBlockPrefab, BLOCK_SPAWN_POSITION, Quaternion.identity) as GameObject;
+                break;
+            case 5:
+                liveBlock = Instantiate(SBlockPrefab, BLOCK_SPAWN_POSITION, Quaternion.identity) as GameObject;
+                break;
+            default:
+            case 6:
+                liveBlock = Instantiate(ZBlockPrefab, BLOCK_SPAWN_POSITION, Quaternion.identity) as GameObject;
+                break;
+        }
+    }
+}
